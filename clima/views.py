4a79ac1772ab51ed52c96dacaab0d78869414901
Leandro090685 <lenife@hotmail.com>
2023-01-19 18:18:48 -0300

    response specific values

--- clima/views.py
+++ clima/views.py
@@ -15,16 +15,26 @@ def current(request, city=None):
         city = datos["city"]
         r2= requests.get("https://wttr.in/"+ city +"?format=j1&lang=es")
         datos2 = r2.json()
-        return JsonResponse(datos2)
+        final = {
+            "Ciudad": datos2["nearest_area"][0]["region"][0]["value"],
+            "Temperatura": datos2["current_condition"][0]["FeelsLikeC"]+"º",
+            "Humedad" : datos2["current_condition"][0]["humidity"]+" %",
+            "Sensacion Termica": datos2["weather"][0]["hourly"][0]["FeelsLikeC"]+"º",
+            }
+        return JsonResponse(final)
     else:
         r3= requests.get("https://wttr.in/"+ city +"?format=j1&lang=es")
         datos3 = r3.json()
-        return JsonResponse(datos3)
+        final2 = {
+            "Ciudad": datos3["nearest_area"][0]["region"][0]["value"],
+            "Temperatura": datos3["current_condition"][0]["FeelsLikeC"]+"º",
+            "Humedad" : datos3["current_condition"][0]["humidity"]+" %",
+            "Sensacion Termica": datos3["weather"][0]["hourly"][0]["FeelsLikeC"]+"º",
+            }
+        return JsonResponse(final2)
+        
 
 def prueba(request):
     return HttpResponse("hola mundo")
-
-
-
     
 
