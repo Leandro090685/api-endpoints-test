1a1a5cea337ebd4b1bebbfbe18d3793ef88cf672
o-andres-m <elhinchon@gmail.com>
2023-01-20 22:05:55 +0100

    Fix code and delete all

--- clima/views.py
+++ clima/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import requests
+from clima.models import Registros
+from django.forms.models import model_to_dict
 
 
 def index(request):
@@ -9,32 +11,31 @@ def index(request):
     return JsonResponse(datos)
 
 def current(request, city=None):
-    if (not city):
+
+    while (not city):
         r = requests.get("http://ip-api.com/json/")
-        datos = r.json()
-        city = datos["city"]
-        r2= requests.get("https://wttr.in/"+ city +"?format=j1&lang=es")
-        datos2 = r2.json()
-        final = {
-            "Ciudad": datos2["nearest_area"][0]["region"][0]["value"],
-            "Temperatura": datos2["current_condition"][0]["FeelsLikeC"]+"º",
-            "Humedad" : datos2["current_condition"][0]["humidity"]+" %",
-            "Sensacion Termica": datos2["weather"][0]["hourly"][0]["FeelsLikeC"]+"º",
-            }
-        return JsonResponse(final)
-    else:
-        r3= requests.get("https://wttr.in/"+ city +"?format=j1&lang=es")
-        datos3 = r3.json()
-        final2 = {
-            "Ciudad": datos3["nearest_area"][0]["region"][0]["value"],
-            "Temperatura": datos3["current_condition"][0]["FeelsLikeC"]+"º",
-            "Humedad" : datos3["current_condition"][0]["humidity"]+" %",
-            "Sensacion Termica": datos3["weather"][0]["hourly"][0]["FeelsLikeC"]+"º",
-            }
-        return JsonResponse(final2)
-        
+        city = r.json()["city"]
+
+    r= requests.get("https://wttr.in/"+ city +"?format=j1&lang=es")
+    data= r.json()
+
+    registro = Registros()
+
+    registro.city = city
+    registro.temperature = data["current_condition"][0]["temp_C"]
+    registro.humidity = data["current_condition"][0]["humidity"]
+    registro.feels_like = data["current_condition"][0]["FeelsLikeC"]
+
+    registro.save()
+    return JsonResponse(model_to_dict(registro))
 
 def prueba(request):
     return HttpResponse("hola mundo")
     
 
+
+def delete(request):
+    Registros.objects.all().delete()
+    return HttpResponse("ok")
+    
+    
