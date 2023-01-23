390392980b000a3255689b64a6f1839ce93d9152
Leandro Ferrari <111808524+Leandro090685@users.noreply.github.com>
2023-01-23 17:13:43 -0300

    first win

--- clima/urls.py
+++ clima/urls.py
@@ -8,17 +8,12 @@ urlpatterns = [
     path("current/",views.current, name='current'),
     path("prueba",views.prueba, name='prueba'),
     path("delete",views.delete, name='delete'),
-<<<<<<< HEAD
     path("registros", RegistrosListview.as_view()),
     path("registros/<pk>", RetrieveUpdateDestroy.as_view()),
     path("consulta",CreateRegistro.as_view()),
     path("consulta/<str:city>",CreateRegistro.as_view()),
-    
-
-=======
     path("all",views.find_all, name='delete'),
     path("delete/<int:id>",views.delete, name='delete'),
->>>>>>> 7c99297c735484a2975a72d963dc9ef436d596ab
 
 ]
 
