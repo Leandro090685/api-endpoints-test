b3312f3352d82d7a45a94201476befde14182d2f
Leandro Ferrari <111808524+Leandro090685@users.noreply.github.com>
2023-01-23 20:21:34 -0300

    services

--- clima/views.py
+++ clima/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponse, JsonResponse
 import requests
 from clima.models import Registros
 from django.forms.models import model_to_dict
-from rest_framework import generics
+from rest_framework import generics,status
 from rest_framework.response import Response 
 from rest_framework.views import APIView
 from clima.serializers.registro_serializer import RegistroSerializer
+from clima.services.service_register import RegisterService
 
 from django.views.decorators.csrf import csrf_exempt
 
@@ -52,22 +53,13 @@ class RegistrosListview(generics.ListAPIView):
     serializer_class = RegistroSerializer
 
 class CreateRegistro(APIView):
-    def get(self, request, city=None, format=None):
-        if (not city):
-            r = requests.get("http://ip-api.com/json/")
-            city = r.json()["city"]
-        r= requests.get("https://wttr.in/"+ city +"?format=j1&lang=es")
-        data= r.json()
-        registro = Registros()
-
-        registro.city = city
-        registro.temperature = data["current_condition"][0]["temp_C"]
-        registro.humidity = data["current_condition"][0]["humidity"]
-        registro.feels_like = data["current_condition"][0]["FeelsLikeC"]
-
-        registro.save()
-
-        return Response(model_to_dict(registro))
+    def get(self,request,city=None,format=None):
+        data = RegisterService.register(city)
+        serializer = RegistroSerializer(data = data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED,data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
             
